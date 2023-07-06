8e276f2dcc2018d45bfc31a24bf031bc90412cc0
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-07-06 14:32:24 -0300

    Initial commit

---
+++ base/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('routes/', views.getRoutes, name='routes'),
+    path('products/', views.getProducts, name='products'),
+    path('products/<str:pk>', views.getProduct, name='product'),
+
+
+]
+
