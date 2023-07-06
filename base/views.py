13e5c71d22c3016bbf29ffed8bd8602fb31f5316
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-07-06 17:33:37 -0300

    Serializing Data

--- base/views.py
+++ base/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Product
 from .products import products
+from .serializers import ProductSerializer
 
 
 @api_view(['GET'])
@@ -22,13 +24,12 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if(i['_id'] == pk ):
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
