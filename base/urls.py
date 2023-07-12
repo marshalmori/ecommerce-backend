fc8eb83da19010341537a653164bc758981a64af
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-07-12 20:06:59 -0300

    Register User

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/register/', views.registerUser, name='register'),
+
     path('', views.getRoutes, name='routes'),
     path('users/profile/', views.getUserProfile, name='users-profile'),
     path('users/', views.getUsers, name='users'),
--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
@@ -40,6 +42,23 @@ def getRoutes(request):
         '/api/products/<update>/<id>/'
     ]
     return Response(routes)
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
