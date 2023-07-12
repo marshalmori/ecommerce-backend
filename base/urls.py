475f467d4e02ba923a15e9c3fdc45fdabafeca48
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-07-12 17:01:13 -0300

    User Serializer

--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('routes/', views.getRoutes, name='routes'),
+    path('', views.getRoutes, name='routes'),
+    path('users/profile/', views.getUserProfile, name='users-profile'),
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>', views.getProduct, name='product'),
 
